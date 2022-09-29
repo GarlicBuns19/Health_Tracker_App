3a089889de2f6cadf5f97ab71096df8cb702f76b
GarlicBuns19 <planterx26@gmail.com>
2022-09-29 10:08:29 +0200

    HTA Navbar

---
+++ src/components/SideBar.vue
@@ -0,0 +1,59 @@
+<template>
+  <nav>
+    <v-navigation-drawer
+      v-model="drawer"
+      dark
+      app
+      mini-variant
+      mini-variant-width="100"
+      class="teal"
+    >
+      <v-list>
+        <v-list-item class="mb-12">
+          <v-list-item-content>
+            <v-icon class="mb-2" large>
+              fas fa-hand-holding-medical
+              <v-list-item-subtitle align="center"> Medi </v-list-item-subtitle>
+            </v-icon>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+      <v-list flat>
+        <v-list-item router to="/">
+          <v-list-item-content>
+            <v-icon class="mb-2">fas fa-home</v-icon>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item router to="/">
+          <v-badge color="pink" dot overlap>
+            <v-list-item-content>
+              <v-icon class="mb-2">fas fa-user</v-icon>
+            </v-list-item-content>
+          </v-badge>
+        </v-list-item>
+        <v-list-item router to="/">
+          <v-list-item-content>
+            <v-icon class="mb-2">fas fa-envelope</v-icon>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item router to="/">
+          <v-list-item-content>
+            <v-icon class="mb-2">fas fa-cog</v-icon>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item router to="/">
+          <v-list-item-content>
+            <v-icon class="mb-2">fas fa-question-circle</v-icon>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </nav>
+</template>
+<script>
+export default {
+  data: () => ({
+    drawer: true,
+  }),
+};
+</script>
